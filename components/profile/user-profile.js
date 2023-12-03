9d359cb4d43b8ee19a50e7cb18b887d15931a025
HAMAD ZIA <hamadzia74@gmail.com>
2023-12-03 14:35:29 +0500

    Added Server-Side Page Guards to Protect the Profile Page

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -4,22 +4,23 @@ import classes from './user-profile.module.css';
 import { useEffect } from 'react';
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getSession().then(session => {
+  // useEffect(() => {
+  //   getSession().then(session => {
 
-      if (!session) {
-        window.location.href = '/auth';
-      } else {
-        setIsLoading(false);
-      }
-    })
-  }, [])
+  //     if (!session) {
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   })
+  // }, [])
 
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>
-  }
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>
+  // }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
